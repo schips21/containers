20cb3c93a66efb4c4d7d4662faf2dd5156c8c2e4
schips <tkpokidova@gmail.com>
2021-03-06 19:50:25 +0300

    started list

---
+++ list.hpp
@@ -0,0 +1,21 @@
+#ifndef LIST_HPP
+# define LIST_HPP
+
+#include <iostream>
+
+namespace ft{class list; class node}
+
+template <typename T>
+class list{
+private:
+	T content;
+	T * prev_elem;
+	T * next_elem;
+
+//	class node{
+//
+//	};
+
+};
+
+#endif
