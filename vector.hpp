a15b04a8cfe1d01e5cf408581ca22d02ea993fcc
schips21 <tkpokidova@gmail.com>
2021-04-22 11:14:35 +0300

    added private members for vector

--- vector.hpp
+++ vector.hpp
@@ -15,6 +15,12 @@ namespace ft{
 		typedef typename allocator_type::const_reference	const_reference;
 		typedef typename allocator_type::pointer			pointer;
 		typedef typename allocator_type::const_pointer	const_pointer;
+
+	private:
+		allocator_type	_alloc;
+		size_type		_size;
+		size_type		_capacity;
+		pointer			_data;
 	};
 }
 
